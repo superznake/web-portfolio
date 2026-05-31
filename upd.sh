0ade82f300401aaa032760c520224941dc342235
superznake <108449247+superznake@users.noreply.github.com>
2026-05-31 18:41:13 +0300

    Update upd.sh

--- upd.sh
+++ upd.sh
@@ -4,4 +4,4 @@ git stash
 git pull origin main
 chmod +x upd.sh
 docker build -t web_portfolio:latest .
-docker run --name web_portfolio -p 127.0.0.1:62000:62000 -v $(pwd):/app web_portfolio:latest
+docker run --name web_portfolio -p 0.0.0.0:62000:62000 -v $(pwd):/app web_portfolio:latest
